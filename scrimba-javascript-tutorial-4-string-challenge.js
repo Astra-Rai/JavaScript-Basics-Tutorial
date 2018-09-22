e3cc59d55c11ce9ebaeb16b14afaad7b7a78f291
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-09-22 14:02:41 -0700

    Concatenate two variables in one variable

---
+++ scrimba-javascript-tutorial-4-string-challenge.js
@@ -0,0 +1,27 @@
+
+
+//instructions set variable fullName = to firstName + lastName
+
+//let firstName = "Astra";
+//let lastName = "Rai";
+
+//function fullName(){
+//  console.log(`${firstName} ${lastName}`);  
+//}
+//console.log(fullName());/
+
+//first I wrote console.log(fullName), as if full namee was a variable
+//changed console.log(fullName), to console.log(fullName()) and code
+//worked :)
+
+//wrong answer below :(
+//let fullName = `${firstName} ${lastName}`;
+//cosole.log (fullName);
+
+//how instructored did exericse
+
+let firstName = "Astra";
+let lastName = "Rai";
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);

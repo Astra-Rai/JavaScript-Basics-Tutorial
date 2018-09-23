c158786e7d87c0d9647a32f2c3dca6e1a5088bdc
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-09-23 10:40:18 -0700

    Add files via upload
    Tutorial on numbers. Review of functions: parseInt, parseFloat, toFixed

---
+++ scrimba-javascript-tutorial-5-numbers.js
@@ -0,0 +1,20 @@
+let example = 7;
+//console.log(example);
+
+let example1 = 7.77;
+//console.log(example1);
+//console.log(typeof example1);
+//will give whole #, interger, note doesn't round #
+//output: "number"
+let example2 = 7.77;
+//console.log(parseInt(example2));
+//able to parse intFloat to a string
+//console.log(typeof example2);
+//outpt: "number"
+
+let example3 = '7.77';
+//console.log(example3);
+//output: "7.77"
+
+let example4 = 7.77;
+//console.log(example4.toFixed(5));

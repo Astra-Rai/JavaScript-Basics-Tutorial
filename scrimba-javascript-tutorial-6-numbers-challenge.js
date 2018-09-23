a2ebe27e1dbff2f476619f4cfa03ea158288879f
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-09-23 10:41:20 -0700

    Add files via upload
    Challenge exercise using functions: parseInt, parseFloat, toFixed

---
+++ scrimba-javascript-tutorial-6-numbers-challenge.js
@@ -0,0 +1,27 @@
+var a = "yo";
+//console.log(a)
+
+let example1 = parseInt("Hello 33 World 22");
+//console.log(example1);
+//output:NaN
+//console.log(typeof example1);
+//output: "number
+//parseInt: passes a string, returns an int
+
+let example2 = parseFloat('44 Dylan 33');
+//console.log(example2);
+//44
+//console.log(typeof example2);
+//output: "number"
+//The parseFloat() function parses an argument and returns a floating point number.
+
+let example3 = 55.3333.toFixed(0);
+//console.log(example3);
+//output: "55"
+//console.log(typeof example3);
+//output: String
+//The toFixed() method formats a number using fixed-point notation.
+
+let example4 = 200.0.toFixed(2);
+//console.log(example4);
+//console.log(typeof example4);

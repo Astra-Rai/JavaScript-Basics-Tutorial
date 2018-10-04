b60d0f44036ec44cebf419142ced9be89dc5d1e2
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-10-04 12:35:56 -0700

    Increment-and-Decrement-Operators
    Increment-and-Decrement-Operators

---
+++ scrimba-javascript-increment-and-decrement-operators.js
@@ -0,0 +1,19 @@
+
+let a = 5;
+a++;
+//expected output: 6
+//console.log(a);a
+--a;
+//expected output: 5 
+//console.log(a);
+
+
+let example1 = 1;
+example1++;
+//expected output: 2;
+//console.log(example1);
+
+
+example1 += 5;
+//expected output: 7;
+//console.log(example1);

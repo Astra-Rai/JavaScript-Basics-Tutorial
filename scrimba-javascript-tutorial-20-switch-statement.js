9802d7b287ead72aaa730ae9b698e9d2c5dbea0f
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-10-04 12:29:35 -0700

    Switch Statement
    Switch Statement

---
+++ scrimba-javascript-tutorial-20-switch-statement.js
@@ -0,0 +1,17 @@
+//switch exericse
+
+let studentAnswer = 'A';
+switch(studentAnswer){
+    
+  case 'A':
+    console.log('A is wrong, sorry homie');
+    break;
+ 
+  case 'B':    
+    //console.log('B is wrong, too');
+    break;
+ 
+  default:////////////
+   console.log('Not a real aswers');
+    
+}

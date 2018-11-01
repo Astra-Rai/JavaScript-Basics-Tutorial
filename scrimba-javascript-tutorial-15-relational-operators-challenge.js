03cb1b51b1e811551a5a98b125a9f9add4deaf8c
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-01 14:26:24 -0700

    Relational Operators Challenge

---
+++ scrimba-javascript-tutorial-15-relational-operators-challenge.js
@@ -0,0 +1,18 @@
+
+let example1 = 5 === 5;
+console.log(example1);//output: true 
+
+let example2 = 5 == '5';
+console.log(example2);//output true
+
+let example3 = 6 != '6';
+console.log(example3);//output false
+
+let example4 = 7 !=='7';
+console.log(example4);//output true
+
+//Notes: 
+/*
+let b = 2, console(b)//output 2
+let c = '2', console.log(c)//ouput '2'
+*/

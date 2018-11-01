d02dbd2d6160e2e3327d359670e3ca37fa2c9143
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-01 13:46:21 -0700

    Arithmetic Operators

---
+++ scrimba-javascript-tutorial-13-Arithemitic-Operators.js
@@ -0,0 +1,25 @@
+//Arithemetic Operators 
+
+//Notes MDN
+/*
+
+Arithmetic operators take numerical values (either literals or variables) as their operands and return a single numerical value. The standard arithmetic operators are addition (+), subtraction (-), multiplication (*), and division (/).
+
+*/
+
+
+
+let example1 = 5 * 5;
+console.log(example1);
+//expected output: 25
+let example2 = 5 * 5 - 4 * 4 /4
+//25 - 4 /1, 21/1 = 21
+//note: order of operations followed
+console.log(example2);
+
+//modulous operator
+/*
+5 % %. 0
+5%3, 2
+
+*/

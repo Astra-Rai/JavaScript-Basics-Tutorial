2b673e7bbb702c54da6d992c1c9f4135ffde059d
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-01 12:13:27 -0700

    Increment and Decrement Part 2

---
+++ scrimba-javascript-tutorial-16-increment and decrement.js
@@ -0,0 +1,19 @@
+//increment, +
+//decrement, -
+
+let example1 = 1;
+example1++;//increment value in variable example1 by 1
+//console.log(example1);
+
+let example2 = 5;
+example2 +=5;
+//console.log(example2);
+
+let example3 = 5;
+++example3;//adds value within line of code
+/*
+if line 13 was example ++, 
+increment would occur after line of code??
+I'm not sure I understand this weell??
+*/
+//console.log(example3);

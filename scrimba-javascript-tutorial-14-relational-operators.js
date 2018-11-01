838545f553a0c45a99c494688909bb5f172da499
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-01 14:13:51 -0700

    Relational Operators

---
+++ scrimba-javascript-tutorial-14-relational-operators.js
@@ -0,0 +1,15 @@
+
+
+let example1 = 10;
+let example2 = 15;
+
+console.log(example1 > example2);
+//output: false
+
+//type of
+//The typeof operator returns a string indicating the type of the unevaluated operand.
+console.log(typeof example1);
+//output: "number"
+
+//==checks of values are the same
+//===checking if value and data are the same
